a37b2ab1966736f68824ec27dac6964265ff8442
Xie Yuheng <xyheme@gmail.com>
2025-11-11 18:54:29 +0800

    up

--- README.md
+++ README.md
@@ -2,30 +2,9 @@
 
 A simple library for building CLI with sub-commands in Node.js.
 
-## Usage
+## Example
 
-```typescript
-import * as cmd from "@xieyuheng/command.js"
-
-const router = cmd.createRouter("calculator", "0.1.0")
-
-const routes = [
-  "add x y -- add two numbers and print the result",
-  "mul --x --y -- mul two numbers and print the result",
-]
-
-router.bind(routes, {
-  add: ([x, y]) => {
-    console.log(Number(x) + Number(y))
-  },
-  mul: (args, options) => {
-    console.log(Number(options["--x"]) * Number(options["--y"]))
-  },
-})
-
-await router.run(["add", "2", "2"])
-await router.run(["mul", "--x", "3", "--y", "3"])
-```
+[](calculator.example.ts)
 
 ## License
 
---
+++ calculator.example.ts
@@ -0,0 +1,43 @@
+import * as Cmd from "./src/index.ts"
+
+function logger(): Cmd.Middleware {
+  return (args, options, tokens, continuation) => {
+    console.log({ args, options, tokens })
+    return continuation(args, options, tokens)
+  }
+}
+
+const router = Cmd.createRouter("calculator", "0.1.0", {
+  middleware: [logger()],
+})
+
+router.defineRoutes([
+  "add x y -- add two numbers (secretly double them) and print the result",
+  "mul --x --y -- mul two numbers and print the result",
+])
+
+function doubleArgs(): Cmd.Middleware {
+  return (args, options, tokens, continuation) => {
+    args = args.map((arg) => String(Number(arg) * 2))
+    return continuation(args, options, tokens)
+  }
+}
+
+router.defineHandlers({
+  add: {
+    middleware: [doubleArgs()],
+    handler([x, y]) {
+      console.log(Number(x) + Number(y))
+    },
+  },
+  mul(args, options) {
+    console.log(Number(options["--x"]) * Number(options["--y"]))
+  },
+})
+
+try {
+  await router.run(process.argv.slice(2))
+} catch(error) {
+  console.log(error)
+  process.exit(1)
+}
